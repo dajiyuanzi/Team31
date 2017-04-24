655371ba27cd481ba4584aff2a2a322b88bd9f27
dajiyuanzi <dajiyuanzi@foxmail.com>
2017-04-24 13:42:32 +0200

    Ji Yuan, database commit

--- database/database.sql
+++ database/database.sql
@@ -1,6 +1,7 @@
 
 create database superkarlskrona;
 
+drop table if exsits 'user';
 
 create table user(
     uid int AUTO_INCREMENT,
@@ -9,3 +10,26 @@ create table user(
     code varchar(250),
     PRIMARY KEY(uid)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS topic;
+
+CREATE table topic (
+	tid int AUTO_INCREMENT,
+	name varchar(250),
+    description varchar(250),
+    code varchar(250),
+    PRIMARY KEY(tid)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS comment;
+
+CREATE table comment (
+	cid int AUTO_INCREMENT,
+    tid int,
+    uid int,
+    comment varchar(250),
+    PRIMARY KEY(cid)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*Don't need foreign keys*/
