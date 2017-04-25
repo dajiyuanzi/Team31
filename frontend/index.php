a649db0a6eb90119288de81defdf415a743827a1
dajiyuanzi <dajiyuanzi@foxmail.com>
2017-04-25 12:14:56 +0200

    Ji Yuan- Navbar, index frontend, project frame

---
+++ frontend/index.php
@@ -0,0 +1,19 @@
+<html>
+	<?php require_once('../public/head.php'); ?>
+	<body class="bg1">
+	<div class="container-fluid">
+		<?php require_once('../public/nav.php'); ?>
+		<div class="row-fluid">
+			<div class="span2" style="background:yellow;">
+				
+			</div>
+			<div class="span8" style="background:red;">
+				
+			</div>
+			<div class="span2" style="background:green;">
+				
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
---
+++ public/nav.php
@@ -0,0 +1,38 @@
+<style type="text/css">
+	.navbg{
+	background:	purple; 
+	opacity:0.4;
+	}
+</style>
+
+<div class="row-fluid">
+	<div class="span12">
+		<ul class="nav nav-tabs navbg">
+			<li class="active">
+				<a href="login.php">Home</a>
+			</li>
+			<li>
+				<a href="index.php">Index</a>
+			</li>
+			<li class="disabled">
+				<a href="comment">Comment</a>
+			</li>
+			<li class="dropdown pull-right">
+				<a href="#" data-toggle="dropdown" class="dropdown-toggle">Drop<strong class="caret"></strong></a>
+				<ul class="dropdown-menu">
+					<li>
+						<a href="register.php">Register</a>
+					</li>
+					<li>
+						<a href="profile.php">Profile</a>
+					</li>
+					<li class="divider">
+					</li>
+					<li>
+						<a href="#">Login Out</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>	
