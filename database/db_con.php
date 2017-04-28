3a6f9f5ce99597c1e3cdcd972b95dac094a8b81b
Chrissesweet <Chrisse.e@hotmail.com>
2017-04-28 21:44:56 +0200

    fix names database onnection

--- database/db_con.php
+++ database/db_con.php
@@ -5,9 +5,9 @@ $password = "";
 $dbname = "superkarlskrona";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$con = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
+if ($con->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
