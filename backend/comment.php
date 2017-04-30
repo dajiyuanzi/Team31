e1f93900c32e25679f7e6e286da3c3427889cc62
dajiyuanzi <dajiyuanzi@foxmail.com>
2017-04-30 16:10:30 +0200

    Update comment.php

--- backend/comment.php
+++ backend/comment.php
@@ -5,14 +5,8 @@ require_once('../public/login_check.php');
 
 $tid=$_GET['tid'];
 $uid=$_SESSION['uid'];
-<<<<<<< HEAD
 
 $result = $con->query("select comment from comment where tid='".$tid."';");
-=======
-$comment=$_POST['comment'];
-
-$result = $con->query("select comment from comment where tid=".$tid.";");
->>>>>>> origin/master
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
@@ -22,28 +16,20 @@ if ($result->num_rows > 0) {
     }
 }
 else {
-   	echo "<div class='topicBox' style='background-color:yellow;' >";
+    echo "<div class='topicBox' style='background-color:yellow;' >";
     echo "  <p>No Comment</p><br>";
     echo "</div>";
 }
 
 if(isset($_POST['comment'])&&!empty($_POST['comment'])){
-<<<<<<< HEAD
 	$comment=$_POST['comment'];
-=======
->>>>>>> origin/master
 	$sql = "INSERT INTO comment(tid, uid, comment) VALUES ('".$tid."', '".$uid."', '".$comment."');";
-    if (!$con->query($sql)) {
-        echo 'Mysql error: ' . mysql_error();
-        exit;
+	if (!$con->query($sql)) {
+        	echo 'Mysql error: ' . mysql_error();
+        	exit;
     }
-<<<<<<< HEAD
     header("Location:../frontend/comment.php?tid=".$tid."");
 }
 
-=======
-    header("Location:../frontend/comment.php");
-}
->>>>>>> origin/master
 ?>
 
