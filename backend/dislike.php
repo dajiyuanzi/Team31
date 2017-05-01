75392de8bee7bac759d8c7444f9ca960bf5acf83
dajiyuanzi <dajiyuanzi@foxmail.com>
2017-05-01 12:30:21 +0200

    set cookie to prevent duplicate like&dislike

--- backend/dislike.php
+++ backend/dislike.php
@@ -2,15 +2,22 @@
 require_once('../database/db_con.php');
 
 if(isset($_POST['tid'])){
-	$sql="select `dislike` from topic where tid='".$_POST['tid']."';";
-	$result=$con->query($sql) or die('MySQL Error: ' . mysqli_error());
-	$row=$result->fetch_assoc();
+	if(isset($_COOKIE["setcookie".$_POST['tid'].""])){//judge cookie to prevent dupicate submission
+    	echo "Have Done";
+    }
+    else{
+		$sql="select `dislike` from topic where tid='".$_POST['tid']."';";
+		$result=$con->query($sql) or die('MySQL Error: ' . mysqli_error());
+		$row=$result->fetch_assoc();
 
-	$row['dislike']++;
-	$sql="update topic set `dislike`='".$row['dislike']."' where tid='".$_POST['tid']."';";
-	$result=$con->query($sql) or die('MySQL Error: ' . mysqli_error());
+		$row['dislike']++;
+		$sql="update topic set `dislike`='".$row['dislike']."' where tid='".$_POST['tid']."';";
+		$result=$con->query($sql) or die('MySQL Error: ' . mysqli_error());
 
-	echo $row['dislike'];
+		setcookie("setcookie".$_POST['tid']."", "1", time()+72000000); //set cookie to prevent dupicate submission
+
+		echo $row['dislike'];
+	}
 }
 else{
 	die('MySQL Error: ' . mysqli_error());
--- backend/like.php
+++ backend/like.php
@@ -2,15 +2,22 @@
 require_once('../database/db_con.php');
 
 if(isset($_POST['tid'])){
-	$sql="select `like` from topic where tid='".$_POST['tid']."';";
-	$result=$con->query($sql) or die('MySQL Error: ' . mysqli_error());
-	$row=$result->fetch_assoc();
+	if(isset($_COOKIE["setcookie".$_POST['tid'].""])){//judge cookie to prevent dupicate submission
+    	echo "Have Done";
+    }
+    else{
+    	$sql="select `like` from topic where tid='".$_POST['tid']."';";
+		$result=$con->query($sql) or die('MySQL Error: ' . mysqli_error());
+		$row=$result->fetch_assoc();
 
-	$row['like']++;
-	$sql="update topic set `like`='".$row['like']."' where tid='".$_POST['tid']."';";
-	$result=$con->query($sql) or die('MySQL Error: ' . mysqli_error());
-
-	echo $row['like'];
+		$row['like']++;
+		$sql="update topic set `like`='".$row['like']."' where tid='".$_POST['tid']."';";
+		$result=$con->query($sql) or die('MySQL Error: ' . mysqli_error());
+		
+		setcookie("setcookie".$_POST['tid']."", "1", time()+72000000); //set cookie to prevent dupicate submission
+		
+		echo $row['like'];
+    }
 }
 else{
 	die('MySQL Error: ' . mysqli_error());
