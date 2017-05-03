5016c55b41e11f39c902d63388fe5205bc5bd73a
Chrissesweet <Chrisse.e@hotmail.com>
2017-05-03 13:04:49 +0200

    added icons for like and dislike

--- backend/comment.php
+++ backend/comment.php
@@ -62,8 +62,9 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     echo "<div class='topicBox' style='background-color:".$row["color"].";' >";
     echo "  <p><a href='../frontend/comment.php?tid=".$row['tid']."'>".$row["description"]."</a><br>Likes: <span id='liketid".$row['tid']."'>".$row["like"]."</span> Dislikes: <span id='disliketid".$row['tid']."'>".$row["dislike"]."</span></p>";
-    echo "  <button style='background-color: green' onclick='like(".$row['tid'].");'>Like</button>";
-    echo "  <button style='background-color: red' onclick='dislike(".$row['tid'].");'>Dislike</button>";
+    echo "  <div style='margin-left: 12px; display: inline-block; ' onclick='like(".$row['tid'].");'><img style='width: 30px;' src='../assets/images/up.png'></div>";
+    echo "  <div style='display: inline-block; width:20px;'></div>";
+    echo "  <div style='display: inline-block;'  onclick='dislike(".$row['tid'].");'><img style='width: 30px;' src='../assets/images/down.png'></div>";
     echo "</div>";
     $color = $row["color"];
   }
