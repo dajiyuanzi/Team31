2f08b32da5e4d5049744c5e3fb2adcca93ab4a89
dajiyuanzi <dajiyuanzi@foxmail.com>
2017-05-03 15:50:55 +0200

    polish the ajax structure

--- frontend/index.php
+++ frontend/index.php
@@ -2,54 +2,7 @@
 	<link href="../assets/css/style.css" rel="stylesheet" type="text/css" />
 	<?php require_once('../public/head.php'); ?>
 
-	<script type="text/javascript">
-		function like(tid){
-			if (window.XMLHttpRequest)
-			{
-				// IE7+, Firefox, Chrome, Opera, Safari
-				x=new XMLHttpRequest();
-			}
-			else
-			{
-				//IE6, IE5
-				x=new ActiveXObject("Microsoft.XMLHTTP");
-			}
-			x.onreadystatechange=function()
-			{
-				if (x.readyState==4 && x.status==200)
-				{
-					document.getElementById("liketid"+tid).innerHTML=x.responseText;
-				}
-			}
-			x.open("POST","../backend/like.php",true);
-			x.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-        	x.send("tid="+tid);
-		}
-
-		function dislike(tid){
-			if (window.XMLHttpRequest)
-			{
-				// IE7+, Firefox, Chrome, Opera, Safari
-				x=new XMLHttpRequest();
-			}
-			else
-			{
-				//IE6, IE5
-				x=new ActiveXObject("Microsoft.XMLHTTP");
-			}
-			x.onreadystatechange=function()
-			{
-				if (x.readyState==4 && x.status==200)
-				{
-					document.getElementById("disliketid"+tid).innerHTML=x.responseText;
-				}
-			}
-			x.open("POST","../backend/dislike.php",true);
-			x.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-        	x.send("tid="+tid);
-		}
-	</script>
-
+	<script type="text/javascript" src="../public/likedislike_ajax.js"></script>
 	<body class="bg1">
 	<div class="container-fluid">
 
