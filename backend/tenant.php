b862943c1a305fa8b29ef3ae7b8c0b2add78202b
dajiyuanzi <dajiyuanzi@foxmail.com>
2017-05-09 01:01:45 +0200

    implement features of user applying, publishing, viewing and closing room sharing

--- backend/tenant.php
+++ backend/tenant.php
@@ -3,8 +3,37 @@ include_once('../database/db_con.php');
 require_once('../public/login_check.php');
 
 
-?>
+if(isset($_GET['act'])&&$_GET['act']=="publish"){
+	if(isset($_POST['address'])&&isset($_POST['description'])&&isset($_POST['contact'])&&isset($_SESSION['uid'])){
+		$sql="insert into room(address, description, contact, uid) values('".$_POST['address']."', '".$_POST['description']."', '".$_POST['contact']."', '".$_SESSION['uid']."');";
+		$con->query($sql);
+		unset($_GET['act']);
+		echo "<script language=JavaScript> location.replace(location.href);</script>";
+	}
+	else{
+		echo "<script>alert('Please Fill Form');</script>";
+	}
+}
+elseif(isset($_GET['act'])&&$_GET['act']=="apply") {
+	if(isset($_POST['description'])&&isset($_POST['contact'])&&isset($_POST['rid'])&&isset($_SESSION['uid'])){
+		$sql="insert into application(description, contact, rid, uid) values('".$_POST['description']."', '".$_POST['contact']."', '".$_POST['rid']."', '".$_SESSION['uid']."');";
+		$con->query($sql);
+		unset($_GET['act']);
+		echo "<script language=JavaScript> location.replace(location.href);</script>";
+	}
+	else{
+		echo "<script>alert('Please Fill Form');</script>";
+	}	
+}
+elseif(isset($_GET['act'])&&$_GET['act']=="close") {
+	@$con->query("delete from room where uid='".$_SESSION['uid']."';") or die('MySQL Error: ' . mysqli_error());
+	@$con->query("delete from application where rid=(select rid from room where uid='".$_SESSION['uid']."');") or die('MySQL Error: ' . mysqli_error());
+	unset($_GET['act']);
+	header("Location:../frontend/tenant.php");
+}
 
+
+?>
 
 
 
