1e907e4a83927c589a4e78f3d7f303ac145b34a7
ShengjieW <2511458183@qq.com>
2017-05-10 23:42:09 +0200

    fix bug

--- frontend/qtdown.php
+++ frontend/qtdown.php
@@ -1,6 +1,6 @@
-<table id="__01" width="1004" border="0" cellpadding="0" cellspacing="0">
+<table id="__01" width="523.5" border="0" cellpadding="0" cellspacing="0">
           <tr>
-            <td width="1004" height="74" ><table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+            <td width="523.5" height="74" ><table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
               <tr>
                 <td height="26" align="center" class="STYLE1">Develop by: Team31 </td>
               </tr>
