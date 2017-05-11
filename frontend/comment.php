667b1c7b6cce054a0a3fbb0023cab44f1938956b
ShengjieW <2511458183@qq.com>
2017-05-11 23:59:05 +0200

    Add expression

--- frontend/comment.php
+++ frontend/comment.php
@@ -20,12 +20,15 @@
 		<br>
 		<legend>Launch Comment</legend>
 		<form action="../frontend/comment.php?tid=<?php echo $tid; ?>" method="post">
-			<p>
+	  <p>
 				<label for="comment" class="label">Your Comment</label>
 				<textarea id="comment" name="comment" type="input" class="input" style="width:100%;"rows="4" cols="50"></textarea>
 			<p/>
-			<p>
-				<input type="submit" name="login" value="Launch Comment" class="left" />
+	  <p>		
+            <form action="Expression.html"> 
+            <input type='button' value='Add Expression'> 
+</form>		
+                <input type="submit" name="login" value="Launch Comment" class="left" />
 			</p>
 		</form>
 	</div>
