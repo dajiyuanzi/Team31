980a754f56a2ede544699f73156cfd92a15b8b4b
Chrissesweet <Chrisse.e@hotmail.com>
2017-05-12 19:45:30 +0200

    update footer again

--- frontend/qtdown.php
+++ frontend/qtdown.php
@@ -1,6 +1,11 @@
-<div class="footer">
-  &copy; Super Karlskrona <br>
-  Developed by: Applied software project management - Team 31 <br>
-  Images from web <br>
-  <?php echo date("Y-m-d");?>
-</div>
+
+<html>
+  <link href="../assets/css/style.css" rel="stylesheet" type="text/css" />
+
+  <div class="footer1">
+    &copy; Super Karlskrona <br>
+    Developed by: Applied software project management - Team 31 <br>
+    Images from web <br>
+    <?php echo date("Y-m-d");?>
+  </div>
+</html>
