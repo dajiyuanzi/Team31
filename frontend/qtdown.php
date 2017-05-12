f6b3a77860d9b08b4f1d6082ccf33e6fe4b906ba
Chrissesweet <Chrisse.e@hotmail.com>
2017-05-12 19:22:48 +0200

    update footer

--- frontend/qtdown.php
+++ frontend/qtdown.php
@@ -1,15 +1,6 @@
-<table id="__01" width="523.5" border="0" cellpadding="0" cellspacing="0">
-          <tr>
-            <td width="523.5" height="74" ><table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-              <tr>
-                <td height="26" align="center" class="STYLE1">Develop by: Team31 </td>
-              </tr>
-              <tr>
-                <td height="26" align="center" bgcolor="#FFFFFF" class="STYLE1">
-                <?php echo date("Y-m-d");?> </td>
-              </tr>
-              
-            </table></td>
-          </tr>
-          
-        </table>
+<div class="footer">
+  &copy; Super Karlskrona <br>
+  Developed by: Applied software project management - Team 31 <br>
+  Images from web <br>
+  <?php echo date("Y-m-d");?>
+</div>
