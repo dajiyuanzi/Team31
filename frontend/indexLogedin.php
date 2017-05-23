414cfcf23197ba07f932b69853a433d6f270a7a1
Chrissesweet <Chrisse.e@hotmail.com>
2017-05-23 14:49:19 +0200

    inital integration of tenant as adverts

--- frontend/indexLogedin.php
+++ frontend/indexLogedin.php
@@ -11,10 +11,8 @@
 		<h1 class="title">Super Karlskrona</h1>
 		<?php require_once('../public/navLogedin.php'); ?>
 		<div class="row-fluid">
-			<!--<div class="span2" style="background:yellow;">
+			<div class="span9">
 
-			</div>
-			<div class="span8">-->
 				<div class="addtopicbutton">
 					<button class="addtopic" onClick="$('.topicform').css('display', 'block'); $('.addtopicbutton').css('display', 'none');">Add topic</button>
 					<br><br>
@@ -32,17 +30,69 @@
 				</div>
 
 				<?php require_once('../backend/topics.php'); ?>
-			<!--</div>
-			<div class="span2" style="background:green;">
 
-			</div>-->
+
+			</div>
+			<div class="sidepanel span3">
+
+							<?php
+								echo "Today is " . date("l") . " " . date("Y-m-d") . "<br>";
+							?>
+							<br>
+							<legend>Adverts</legend>
+
+
+
+							<div class="addtenantbutton">
+								<button class="addtenant" onClick="$('.tenantform').css('display', 'block'); $('.addtenantbutton').css('display', 'none');">Add Advert</button>
+								<br><br>
+							</div>
+							<div style="display:none;" class="tenantform">
+
+									<?php //require_once('../backend/tenant.php'); ?>
+
+									<?php
+										$result = $con->query("select * from room where uid='".$_SESSION['uid']."';");
+										if($result->num_rows > 0){
+											echo "<p>You have published an advert already, check your advert page for more information.</p>";
+											echo " <button onClick='$('.tenantform').css('display', 'none'); $('.addtenantbutton').css('display', 'block');'>Cancel</button><br><br>";
+										}
+										else{
+											echo '<form action="../frontend/tenant.php?act=publish" name="tenantfrom" id="tenantform" method="post">
+																<p>
+																	<label for="description" class="label">Description</label>
+																	<textarea form="tenantform" id="description" name="description" style="width:100%;"rows="4" cols="50"></textarea>
+																<p/>
+																<p>
+																	<label for="address" class="label">Address</label>
+																	<input type="text" name="address" class="left" />
+																<p/>
+																<p>
+																	<label for="contact" class="label">Contact</label>
+																	<input type="text" name="contact"  class="left" />
+																<p/>
+																<p>
+																	<input type="submit" value="Submit" name="publish" class="left" />';
+																echo " <button type='cancel' onClick='$('.tenantform').css('display', 'none'); $('.addtenantbutton').css('display', 'block');'>Cancel</button>
+																</p>
+											</form>";
+										}
+									?>
+
+
+
+
+							</div>
+
+							<?php require_once('../backend/advert.php'); ?>
+
+				</div>
+
 		</div>
 	</div>
  <?php include_once 'qtdown.php';?>
 
 	</body>
-	<br>
-	<br>
 	<br>
 	<br>
 </html>
