781a914fbe03b2632c0ac07e9038e60d31269a53
Chrissesweet <Chrisse.e@hotmail.com>
2017-05-23 23:30:42 -0700

    removing online dependencies

--- public/head.php
+++ public/head.php
@@ -2,8 +2,8 @@
 	<title>Super Karlskrona</title>
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/jquery-2.0.0.min.js"></script>
-	<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/jquery-ui"></script>
-	<link href="http://www.francescomalagrino.com/BootstrapPageGenerator/3/css/bootstrap-combined.min.css" rel="stylesheet" media="screen">
-	<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../online/jquery-2.0.0.min.js"></script>
+	<script type="text/javascript" src="../online/jquery-ui"></script>
+	<link href="../online/bootstrap-combined.min.css" rel="stylesheet" media="screen">
+	<script type="text/javascript" src="../online/bootstrap.min.js"></script>
 </head>
